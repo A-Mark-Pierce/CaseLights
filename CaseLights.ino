15f78a2420c593d211340b033621225a233f1d13
Mark Pierce <Mark Pierce>
2016-12-02 17:19:53 +0100

    Simple test serial comms

---
+++ CaseLights.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // Serielle Schnittstelle initialisieren
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  while ( Serial.available() ) {
+    
+    Serial.write( Serial.read() );
+  }
+
+}
