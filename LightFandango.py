039549724c43d14f7b286d8dd7c4ecc6fc790f33
A-Mark-Pierce <mark@pierce.de>
2016-12-11 12:35:39 +0100

    Delay and repeat for Python helper

--- LightFandango.py
+++ LightFandango.py
@@ -11,7 +11,7 @@
 import sys
 import serial
 import time
-
+import re
 import argparse
 
 parser = argparse.ArgumentParser(description='Send text commands from file to CaseLight controller.')
@@ -21,6 +21,7 @@ parser.add_argument("port", help="COM port to which the CaseLight controller is 
 parser.add_argument("-v", "--verbose", help="Verbose mode, displays all commands sent", action="store_true")
 parser.add_argument("-b", "--baudrate", type=int, default=9600, help="Baud rate for serial comms")
 parser.add_argument("-d", "--delay", type=int, default=3, help="Delay in seconds after opening serial port, to allow Arduino to reset")
+parser.add_argument("-r", "--repeat", type=int, default=1, help="Number of times to repeat sending the file, 0 == infinite")
 args = parser.parse_args()
 
 
@@ -34,41 +35,49 @@ while (countDown > 0):
 	time.sleep(1)
 	countDown = countDown - 1
 
-lineCounter = 0
-with open(args.source, "r") as fileInput:
-	for lightCmd in fileInput:
-		lineCounter = lineCounter + 1
-		lightCmd = lightCmd.strip()
-		
-		if ( (lightCmd == "") or (lightCmd[0] == "#") ):
-			continue
-			
-		if (args.verbose):
-			print( lightCmd )
-			
-		lightCmd += "\r"	
-		serArduino.write(lightCmd.encode("ascii"))
-		response=""
-		while ( True ):
-			respChar = serArduino.read(1)
-			if (respChar <= b"\x80" ):
-			
-				if (respChar == b"" ):
-					break
-					
-				if (respChar == b"\r"):
-#					print( response )
-					response = ""
-				elif (respChar != b"\n"):
-					response += respChar.decode("ascii")
-#					print(response)
+numRepeat = args.repeat
 
-				if (response == "OK"):
-					break
-
-				if ((response == "E_CMD") or (response == "E_LEN")):
-					print( "Error at line " + str(lineCounter) + " : " + lightCmd )
-					break
-					
+while ( (0 == args.repeat) or (numRepeat > 0) ):
+	lineCounter = 0
+	with open(args.source, "r") as fileInput:
+		for lightCmd in fileInput:
+			lineCounter = lineCounter + 1
+			lightCmd = lightCmd.strip()
+			
+			if ( (lightCmd == "") or (lightCmd[0] == "#") ):
+				continue
 				
+			if (args.verbose):
+				print( lightCmd )
+				
+			if (lightCmd[0] == "!"):
+				if ("!DELAY" == lightCmd[0:6].upper()):
+					nDelay = int(re.search(r'\d+', lightCmd).group())
+					time.sleep(nDelay)
+			else:
+				lightCmd += "\r"	
+				serArduino.write(lightCmd.encode("ascii"))
+				response=""
+				while ( True ):
+					respChar = serArduino.read(1)
+					if (respChar <= b"\x80" ):
+					
+						if (respChar == b"" ):
+							break
+							
+						if (respChar == b"\r"):
+							response = ""
+						elif (respChar != b"\n"):
+							response += respChar.decode("ascii")
+
+						if (response == "OK"):
+							break
+
+						if ((response == "E_CMD") or (response == "E_LEN")):
+							print( "Error at line " + str(lineCounter) + " : " + lightCmd )
+							break
+					
+	numRepeat = numRepeat - 1
+	
+time.sleep(1)
 serArduino.close();
